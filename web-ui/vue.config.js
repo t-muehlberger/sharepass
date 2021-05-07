b6735951bc277321b16e41c1efec465d431da765
Tobias Mühlberger <tobias@muehlberger.dev>
2021-05-07 21:46:21 +0200

    setup web local dev ssl certs

--- web-ui/vue.config.js
+++ web-ui/vue.config.js
@@ -1,7 +1,13 @@
+const fs = require('fs')
+
 module.exports = {
   lintOnSave: false,
   assetsDir: 'static',
   devServer: {
-    proxy: 'http://localhost:5000'
+    proxy: 'http://localhost:5000',
+    https: {
+      key: fs.readFileSync('./localhost-key.pem'),
+      cert: fs.readFileSync('./localhost.pem'),
+    },
   }
 }
