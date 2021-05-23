30eb641b1515b14c233438e62a635808d359f6f4
Tobias Mühlberger <tobias@muehlberger.dev>
2021-05-23 20:29:58 +0200

    fixed build

--- web-ui/vue.config.js
+++ web-ui/vue.config.js
@@ -1,13 +1,22 @@
 const fs = require('fs')
 
+function getHttpsConfig() {
+  try {
+    return {
+      key: fs.readFileSync('./localhost-key.pem'),
+      cert: fs.readFileSync('./localhost.pem'),
+    }
+  } catch {
+    console.log("failed to load dev keys")
+    return undefined;
+  }
+}
+
 module.exports = {
   lintOnSave: false,
   assetsDir: 'static',
   devServer: {
     proxy: 'http://localhost:5000',
-    https: {
-      key: fs.readFileSync('./localhost-key.pem'),
-      cert: fs.readFileSync('./localhost.pem'),
-    },
+    https: getHttpsConfig(),
   }
 }
